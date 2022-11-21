b7d8a016acc959bb3a290762544276324a997ea7
Abdulloh <abdullohabduhamidov@gmail.com>
2022-11-21 19:36:31 +0500

    first commit

---
+++ total.js
@@ -0,0 +1,98 @@
+// alert("welcome");
+// var name = prompt("name");
+// var ssss = `salom ${name}`;
+// var surname = prompt("surname");
+// var watisyourname = `salom ${surname}`;
+// var emall = prompt("emall");
+// var emall2 = `your ${emall}`;
+// var all = alert(
+//   `your name : ${name} \n your surname : ${surname} \n your emall ${emall}`
+// );
+// console.log(ssss, watisyourname, emall2);
+// var a = 5;
+// var b = 10;
+// console.log(a + b);
+// var a = 25;
+// var b = 15;
+// var result = a - b;
+// console.log(result);
+// var a = 10;
+// var b = 5;
+// var c = a - b;
+// console.log(c);
+// var a = 10;
+// var b = 3;
+// console.log(a % b);
+// var a = 2;
+// var b = 10;
+// console.log(a ** b);
+// document.getElementById("demo").style.fontSize = "35px";
+
+// juft va toq sonlar
+
+// const evenOnaski = function (nambe) {
+//   if (nambe % 2 === 0) {
+//     console.log(`${nambe} juft`);
+//   } else {
+//     console.log(`${nambe} toq`);
+//   }
+// };
+// evenOnaski(10);
+
+// o'zgarivchilar o'zidan kota sonlar ikkinchi usil
+
+// const maxExp = (namber1, namber2) => {
+//   if (namber1 >= namber2) {
+//     return namber1;
+//   }
+//   return namber2;
+// };
+// console.log(maxExp(10, 8));
+// console.log(maxExp(20, 10));
+
+// o'zgarivchi o'zidan kata sonlar birinchi usill
+
+// const maxAroow = function (namber1, namber2) {
+//   if (namber1 >= namber2) {
+//     return namber1;
+//   }
+//   return namber2;
+// };
+// console.log(maxAroow(5, 4));
+// console.log(maxAroow(11, 8));
+
+// teskar stiring ""
+
+// const resualtDec = function (namber) {
+//   const namberStr = String(namber);
+//   const resualtdStr = namberStr.split("").reverse().join("");
+//   return Number(resualtdStr);
+// };
+// console.log(resualtDec(342));
+
+// arr bir nechta hamasi bitada
+// const arrbox = ["js", "html", "css", "scss"];
+// console.log(arrbox);
+
+// arr gurup
+
+// const array1 = [1, 4];
+// const array2 = [2, 3, 7];
+// const result = array1.concat(array2);
+// console.log(array1);
+// console.log(array2);
+// console.log(result);
+
+// objiktlar
+// const dastiurchi = {
+//   ism: "abdulloh",
+//   yosh: 17,
+//   manzil: {
+//     shahari: "toshkint",
+//     yashshjoyi: "sergili",
+//     dom: 26,
+//     hom: 33,
+//   },
+// };
+// console.log(dastiurchi);
+// console.log(dastiurchi.manzil);
